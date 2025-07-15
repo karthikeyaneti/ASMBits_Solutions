b0912fd46023d52df2dd23c55a7caab2b6b6646c
karthikeyaneti <karthikneti17@gmail.com>
2025-07-15 23:57:51 +0530

    Memory Operations

---
+++ 1 - ARMv7/03 - Memory Operations/7 - Unaligned load.s
@@ -0,0 +1,19 @@
+.data
+Pointer: .word 1234
+.text
+.global _start
+_start:
+    ldr r0, =Pointer
+    bl load
+    1: b 1b    // Done
+
+.global load
+load:
+	ldrb	r3, [r0, #3]
+    ldrb 	r2, [r0, #2]
+    ldrb 	r1, [r0, #1]
+    ldrb 	r0, [r0]
+    orr 	r0, r1, lsl #8
+    orr 	r0, r2, lsl #16
+    orr 	r0, r3, lsl #24
+    bx 		lr
