aec827223c9c585cb0e5df584932e1547fc4cc7b
karthikeyaneti <karthikneti17@gmail.com>
2025-07-25 23:07:39 +0530

    Loops and arrays

---
+++ 1 - ARMv7/06 - Loops and arrays/01 - String length.s
@@ -0,0 +1,22 @@
+.data
+MyString: .string "Hello World"
+
+.text
+.global _start
+_start:
+    ldr r0, =MyString
+    bl strlen
+    1: b 1b
+
+.global strlen
+strlen:
+
+	mov 	r1, r0
+loop:
+	ldrb 	r2, [r0], #1
+    cmp 	r2, #0
+    bne 	loop
+    sub 	r0, r0, r1
+    sub 	r0, r0, #1
+    bx 		lr
+	
